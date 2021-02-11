4403be2ee517d105c923cde30cfbbbc20fa0ecc8
Evan <evan_quirk@me.com>
2021-02-11 23:58:22 +0000

    build breed description fetcher from cat api

---
+++ breedFetcher.js
@@ -0,0 +1,24 @@
+const request = require('request');
+
+const fetchBreedDescription = function(breedName, callback) {
+
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(url, (error, resp, body) => {
+    if (error) {
+      callback(`Error Message: ${error}`,null);
+    }
+
+    const data = JSON.parse(body);
+    // console.log(data);
+
+    const breed = data[0];
+    if (breed) {
+      callback(null,breed.description);
+    } else {
+      callback(`The breed requested: "${breedName}" cannot be found.`,null);
+    }
+  });
+
+};
+
+module.exports = { fetchBreedDescription };
